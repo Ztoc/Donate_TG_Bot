8abae77ad7c57ee1c1612d64a86864ad960d9706
Ryder <bhu0444@gmail.com>
2024-07-03 07:43:30 -0400

    feat: add done page

--- app/done/page.tsx
+++ app/done/page.tsx
@@ -2,9 +2,10 @@
 
 import ClipBoard from "@/components/icons/ClipBoard";
 import Image from "next/image";
-import React from "react";
+import React, { useState } from "react";
 
 const Done = () => {
+  const [show, setShow] = useState(false);
   return (
     <div className="flex flex-col mt-[120px] items-center px-4">
       <Image src={"/assets/dust.png"} width={181} height={101} alt="dust" />
@@ -17,9 +18,40 @@ const Done = () => {
         <div
           onClick={(e) => {
             navigator.clipboard.writeText("Ssd1398fd128RD");
+            setShow(true);
+            setTimeout(() => {
+              setShow(false);
+            }, 1000);
           }}
+          className="relative"
         >
           <ClipBoard />
+          <div
+            className={`absolute right-0 -top-full bg-donate-text-dark text-white p-1 text-xs rounded-md w-[130px] ${
+              show ? "" : " hidden"
+            }`}
+          >
+            Copied to clipboard
+          </div>
+        </div>
+      </div>
+      <div className="mt-[46px] w-full">
+        <div className="font-bold text-[12px]/[25px] text-[#111111]">
+          Psst...Would you like to donate to Toncinerator?
+        </div>
+        <div className="mt-2">
+          <button
+            className={`w-full py-[14px] text-center bg-donate-blue font-semibold text-base text-white rounded-[9px]`}
+          >
+            Yes!
+          </button>
+        </div>
+        <div className="mt-4">
+          <button
+            className={`w-full py-[14px] text-center text-donate-blue font-semibold text-base bg-white rounded-[9px] border border-[#0098EA]`}
+          >
+            Maybe Later...
+          </button>
         </div>
       </div>
     </div>
